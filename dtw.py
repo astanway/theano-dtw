e10e5856cf53d00a0bfc325512d91880ae35d732
Abe Stanway <me@abe.is>
2014-11-14 04:41:28 +0000

    Clean up names

--- dtw.py
+++ dtw.py
@@ -1,50 +1,50 @@
-from theano import function, config, shared, sandbox, scan
+from theano import function, scan
 import theano.tensor as T
 import numpy as np
-#cimport numpy as np
-import time
 
-
-def distance_real(i, square, j):
-    cost = T.abs_(t_x[i] - t_y[j])
-    sample = T.stack(square[i, j+1], square[i+1, j], square[i, j])
-    dist = cost + T.min(sample)
+def inner(i, square, j, vec1, vec2):
+    cost = T.abs_(vec1[i] - vec2[j])
+    stack = T.stack(square[i, j+1], square[i+1, j], square[i, j])
+    dist = cost + T.min(stack)
     square = T.set_subtensor(square[i+1, j+1], dist)
 
     return square
 
 
-def distance(j, square, seq):
-    cost, _ = scan(fn=distance_real,
+def outer(j, square, inner_loop, vec1, vec2):
+    cost, _ = scan(fn=inner,
                     outputs_info=[dict(initial=square, taps=[-1])],
-                    non_sequences=j,
-                    sequences=seq)
+                    non_sequences=[j, vec1, vec2],
+                    sequences=inner_loop)
     return cost[-1]
 
 
-# Compiled
-square = T.dmatrix('square')
-t_x = T.dvector('x')
-t_y = T.dvector('y')
-length = T.dscalar('length')
-seq = T.arange(length, dtype='int64')
-print square
-cost, _ = scan(fn=distance,
-                outputs_info=[dict(initial=square, taps=[-1])],
-                non_sequences=seq,
-                sequences=seq)
+def dtw(array1, array2):
+    s = np.zeros((array1.size+1, array2.size+1))
 
-theano_square = function([t_x, t_y, square, length], cost, on_unused_input='warn')
+    s[:,0] = 100
+    s[0,:] = 100
+    s[0,0] = 0.0
 
-# Input
-n_y = np.array([1.0,1.0,1.0,6.0,1.0])
-n_x = np.array([1.0,1.0,5.0,1.0,1.0])
-nrows = n_x.size
-ncols = n_y.size
-s = np.zeros((n_x.size+1, n_y.size+1))
+    square = T.dmatrix('square')
+    vec1 = T.dvector('vec1')
+    vec2 = T.dvector('vec2')
+    vec1_length = T.dscalar('vec1_length')
+    vec2_length = T.dscalar('vec2_length')
+    outer_loop = T.arange(vec1_length, dtype='int64')
+    inner_loop = T.arange(vec2_length, dtype='int64')
 
-s[:,0] = 100
-s[0,:] = 100
-s[0,0] = 0.0
+    cost, _ = scan(fn=outer,
+                    outputs_info=[dict(initial=square, taps=[-1])],
+                    non_sequences=[inner_loop, vec1, vec2],
+                    sequences=outer_loop)
 
-print theano_square(n_x, n_y, s, nrows)
+    theano_square = function([vec1, vec2, square, vec1_length, vec2_length], cost, on_unused_input='warn')
+
+    return theano_square(array1, array2, s, array1.size, array2.size)
+
+
+if __name__=="__main__":
+    array1 = np.array([1.0,1.0,5.0,1.0,1.0])
+    array2 = np.array([1.0,1.0,1.0,6.0,1.0])
+    print dtw(array1, array2)
