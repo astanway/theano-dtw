3689c265132673f488d445a0ab9e9d66da5836f7
Abe Stanway <me@abe.is>
2014-11-14 04:49:20 +0000

    Docs

--- dtw.py
+++ dtw.py
@@ -3,6 +3,9 @@ import theano.tensor as T
 import numpy as np
 
 def inner(i, square, j, vec1, vec2):
+    """
+    The inner loop that calculates the actual warp path at i,j
+    """
     cost = T.abs_(vec1[i] - vec2[j])
     stack = T.stack(square[i, j+1], square[i+1, j], square[i, j])
     dist = cost + T.min(stack)
@@ -10,22 +13,28 @@ def inner(i, square, j, vec1, vec2):
 
     return square
 
-
 def outer(j, square, inner_loop, vec1, vec2):
+    """
+    The outer loop that calculates warp paths per row
+    """
     cost, _ = scan(fn=inner,
                     outputs_info=[dict(initial=square, taps=[-1])],
                     non_sequences=[j, vec1, vec2],
                     sequences=inner_loop)
     return cost[-1]
 
-
 def dtw(array1, array2):
+    """
+    Accepts: two one dimensional arrays
+    Returns: (float) DTW distance between them.
+    """
     s = np.zeros((array1.size+1, array2.size+1))
 
     s[:,0] = 100
     s[0,:] = 100
     s[0,0] = 0.0
 
+    # Set up symbolic variables
     square = T.dmatrix('square')
     vec1 = T.dvector('vec1')
     vec2 = T.dvector('vec2')
@@ -34,17 +43,22 @@ def dtw(array1, array2):
     outer_loop = T.arange(vec1_length, dtype='int64')
     inner_loop = T.arange(vec2_length, dtype='int64')
 
+    # Run the outer loop
     cost, _ = scan(fn=outer,
                     outputs_info=[dict(initial=square, taps=[-1])],
                     non_sequences=[inner_loop, vec1, vec2],
                     sequences=outer_loop)
 
+    # Compile the function
     theano_square = function([vec1, vec2, square, vec1_length, vec2_length], cost, on_unused_input='warn')
 
-    return theano_square(array1, array2, s, array1.size, array2.size)
-
+    # Call the compiled function and return the actual distance
+    return theano_square(array1, array2, s, array1.size, array2.size)[-1][array1.size, array2.size]
 
 if __name__=="__main__":
+    """
+    Example usage: `python dtw.py`
+    """
     array1 = np.array([1.0,1.0,5.0,1.0,1.0])
     array2 = np.array([1.0,1.0,1.0,6.0,1.0])
     print dtw(array1, array2)
